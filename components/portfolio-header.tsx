a955ecc7cbcfdb66ce6575e0ccab3546fac5398b
shivaKotagiri <shivakotagiri532@gmail.com>
2026-05-04 14:49:21 +0530

    added mail

--- components/portfolio-header.tsx
+++ components/portfolio-header.tsx
@@ -2,6 +2,8 @@
 
 import Image from "next/image";
 import { SplitTextAnimation } from "./ui/split-text-animation";
+import Link from "next/link";
+import { Mail } from "lucide-react";
 
 export function PortfolioHeader() {
 
@@ -10,7 +12,7 @@ export function PortfolioHeader() {
         <div className="flex flex-col gap-0 leading-none">
           <div>
             <span className="sm:text-[4rem] text-[1.75rem] text-balance font-black block tracking-tighter sm:max-w-md max-w-50">
-              Hi, I&apos; am Shiva Kumar Kotagiri
+              Hi, I&apos; am Shiva
             </span>
           </div>
           <SplitTextAnimation 
@@ -18,6 +20,9 @@ export function PortfolioHeader() {
             className={"mt-2 text-base leading-7 text-justify max-w-60 sm:max-w-md text-muted-foreground sm:text-xl"} 
             sentence={"Building web applications, active on Twitter. Learning in public"} 
           />
+          <Link className="flex gap-2 hover:underline underline-offset-2 mt-3" href="mailto:shivakumarkotagiriofficial@gmail.com">
+            <Mail className="w-4! h-4!" /> shivakumarkotagiriofficial@gmail.com
+          </Link>
         </div>
         <Image 
           src={"/me.jpg"} 
